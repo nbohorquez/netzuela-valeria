80ffe42c6562bc65e0cc2a52442468475aa95e7d
Nestor Bohorquez <tca7410nb@gmail.com>
2011-12-10 22:01:43 +0000

    En el "commit" anterior arregle un monton de problemas de uso de memoria del programa:
    *- DataTable: no podian liberarse de todas las referencias de DataGrid.
    *- SecureString: empleados en AutentificacionView y AutentificacionViewModel.
    *- NodoViewModel y Nodo: Eliminacion de hijos y padres sin generar conflictos; desafiliacion de los delegados de cambio de columnas del evento correspondiente de MapeoDeColumnas.
    
    git-svn-id: http://jarcob:8080/svn/Valeria/LineaBase@87 c6fa9037-3a57-144d-b1f9-ecfd5aa6a12e

--- Datos/ProxyDinamico.cs
+++ Datos/ProxyDinamico.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 using WcfSamples.DynamicProxy;              // DynamicProxyFactory
 using System.Data;                          // DataSet
-using System.ServiceModel;                  //
+using System.ServiceModel;                  // WSHttpBinding
 using System.ServiceModel.Description;      // ServiceEndpoint
 
 namespace Zuliaworks.Netzuela.Valeria.Datos
@@ -126,3 +126,7 @@ namespace Zuliaworks.Netzuela.Valeria.Datos
         #endregion
     }
 }
+
+// http://www.google.co.ve/search?hl=es&client=firefox-a&hs=nJJ&rls=org.mozilla:es-ES:official&q=+site:social.msdn.microsoft.com+WCF+DynamicProxy+Library&sa=X&ei=txfjTp-0KrLE0AGKmoDNBQ&sqi=2&ved=0CDoQrQIwAg&biw=1366&bih=606#sclient=psy-ab&hl=es&client=firefox-a&hs=6JJ&rls=org.mozilla:es-ES%3Aofficial&source=hp&q=site:social.msdn.microsoft.com+WCF+DynamicProxy+Library+asynchronous&pbx=1&oq=site:social.msdn.microsoft.com+WCF+DynamicProxy+Library+asynchronous&aq=f&aqi=&aql=&gs_sm=e&gs_upl=29517l32601l0l32802l13l11l0l0l0l0l284l2401l0.3.8l11l0&bav=on.2,or.r_gc.r_pw.,cf.osb&fp=ecddcbf5a724ca0a&biw=1366&bih=606
+// http://social.msdn.microsoft.com/Forums/en-US/wcf/thread/8cacd2e6-c817-4dd4-acc1-887c074ae3fe/
+// http://social.msdn.microsoft.com/Forums/en/windowsphone7series/thread/7697d769-782c-4f38-b930-8fbaa38833e1

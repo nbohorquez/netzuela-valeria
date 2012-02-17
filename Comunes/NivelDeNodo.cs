66d7f66f6163a3446fa20c66eb56111f95da0836
Nestor Bohorquez <tca7410nb@gmail.com>
2012-02-17 02:19:27 -0430

    Separe la clase Constantes.cs en varias clases independientes. Cree Desechable.cs para que implemente IDisposable con el metodo recomendado (ver el codigo).

---
+++ Comunes/NivelDeNodo.cs
@@ -0,0 +1,17 @@
+﻿namespace Zuliaworks.Netzuela.Valeria.Comunes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public static class NivelDeNodo
+    {
+        public const int Nulo = -1;
+        public const int Raiz = 0;
+        public const int Servidor = 0;
+        public const int BaseDeDatos = 1;
+        public const int Tabla = 2;
+        public const int Columna = 3;
+    }
+}
---
+++ Comunes/SGBDR.cs
@@ -0,0 +1,24 @@
+﻿namespace Zuliaworks.Netzuela.Valeria.Comunes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public delegate void ExpresionGenerica();
+
+    /// <summary>
+    /// SGBDR: Sistemas Gestores de Bases de Datos Relacionales (RDBMS en inglés)
+    /// </summary>
+    public static class SGBDR
+    {
+        public const string Netzuela = "Netzuela";          // No es realmente un SGBDR pero...
+        public const string SqlServer = "SQL Server";
+        public const string MySQL = "MySQL";
+        public const string Oracle = "Oracle";
+        public const string Access = "Access";
+        public const string Db2 = "DB2";
+        public const string DbIsam = "DBISAM";
+        public const string Predeterminado = "Predeterminado";
+    }
+}
